c0cb34e2e0131bb9337411efe04e048c3cbb02a6
Microchills <zhangzhuang@163.sufe.edu.cn>
2022-09-14 00:30:59 +0800

    Add save function

--- intershipInfo.py
+++ intershipInfo.py
@@ -1,4 +1,3 @@
-#from urllib import response
 import requests
 import logging
 from lxml import etree
@@ -7,14 +6,14 @@ import aiohttp
 import time
 import json
 import re
-
-
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s: %(message)s')
+import pandas as pd
 
 # scrape page and print log and error messages
-def scrape_page(url,params=''):
-    logging.info('Scraping %s...',url)
+def scrape_page(url,page,params=''):
+    logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s: %(message)s')
+
+    logging.info('Scraping %s...',url+'   Page'+str(page))
     headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.27'
         }
@@ -29,27 +28,28 @@ def scrape_page(url,params=''):
 
 # save the job information scraped to JSON file
 def save_jobData(job_name,job_salary,job_week,job_time,job_details,
-                job_location,com_name,com_desc):
+                job_location,com_name,com_desc,dataList):
     jobData = {
         'job_name': job_name,
         'job_salary': job_salary,
         'job_week': job_week,
         'job_time': job_time,
         # eliminating the '\n''\t' among the information and removing blank
-        'job_details': [deta for deta in [re.sub('\\s', '',detail) 
-                       for detail in job_details] if deta != ''],
+        'job_details': ''.join([deta for deta in [re.sub('\\s', '',detail) 
+                       for detail in job_details] if deta != '']),
         'job_location': job_location,
         # aviod out of range error
         'com_name': re.sub('\\s','',''.join(com_name)),
         'com_desc': com_desc,
     }
-    with open('jobData.txt', 'a+',encoding='utf-8',) as f:
-        f.write(json.dumps(jobData,indent=4,ensure_ascii=False))
-        f.write(',' + '\n')
+    dataList.append(jobData)
+    # with open('jobData.txt', 'a+',encoding='utf-8',) as f:
+    #     f.write(json.dumps(jobData,indent=4,ensure_ascii=False))
+    #     f.write(',' + '\n')
 
 # scrape page of detailed information 
 # defined as coroutine object to improve efficiency
-async def scrape_infoPage(url):
+async def scrape_infoPage(url,dataList):
     async with aiohttp.ClientSession() as session:
         async with await session.get(url) as response:
             html = etree.HTML(await response.text())
@@ -63,17 +63,17 @@ async def scrape_infoPage(url):
             com_name = html.xpath('//a[@class="com-name"]/text()')
             com_desc = html.xpath('string(//a[@class="com-name"]/following-sibling::div)')
             save_jobData(job_name,job_salary,job_week,job_time,job_details,
-                        job_location,com_name,com_desc)
+                        job_location,com_name,com_desc,dataList)
 
 # scrape page of intern job list
 def scrape_listPage(page):
     url = 'https://www.shixiseng.com/interns'
     params = {
         'page': 'page',
-        'keyword': '基金',
+        'keyword': '证券',
         'city': '上海'
         }
-    html = scrape_page(url, params)
+    html = scrape_page(url,page,params)
     urlList = html.xpath('//div[@class="f-l intern-detail__job"]//a/@href')
     if page == 1:
         # get the max page number at the first scraping
@@ -84,18 +84,25 @@ def scrape_listPage(page):
 
     
 def main():
-    #scrape the first page
     start = time.time()
+    #scrape the first page
     page = 1
     urlList,last_page = scrape_listPage(page)
     #while the number of page is more than 1,continue scraping
     while (page < last_page ):
         page += 1
         urlList.extend(scrape_listPage(page))
-    print("Saving Data...")
+    print("Scraping detailed information...")
+    dataList = []
     loop = asyncio.get_event_loop()
-    tasks = [asyncio.ensure_future(scrape_infoPage(url)) for url in urlList]
+    tasks = [asyncio.ensure_future(scrape_infoPage(url,dataList)) for url in urlList]
     loop.run_until_complete(asyncio.wait(tasks))
+    #save data to jobData.csv
+    df = pd.DataFrame(dataList)
+    df.index += 1
+    df.to_csv("jobData.csv",encoding="utf-8-sig")
+    # with open('jobData_test.json', 'a+',encoding='utf-8') as f:
+    #    f.write(json.dumps(dataList,indent=4,ensure_ascii=False))
     end = time.time()
     print("Number of data scraped: ",len(urlList))
     print("Cost time: ", end - start)
